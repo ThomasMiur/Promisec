f428c428a5cd452e1706f785e3132952ffcdc760
ThomasMiur <103307255+ThomasMiur@users.noreply.github.com>
2022-06-26 20:20:10 +0400

    Домашка промис

---
+++ promises/timeout.js
@@ -0,0 +1,11 @@
+/**
+ * @param {string} name
+ */
+ const delayedWelcome = name => {
+	// your code
+  console.log(`Welcome ${name}`);
+}
+
+// Sample usage - do not modify
+delayedWelcome("Sam");
+delayedWelcome("Alex");
